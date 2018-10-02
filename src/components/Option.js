025c99e05e2047410af80b3c4bde61d48e67e28b
DanBonehill <DanBonehill@me.com>
2018-10-02 09:23:16 +0100

    Refactor stateless functional components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const Option = (props) => {
-    return (
-        <div>
-            {props.option}
-            <button
-                onClick={() => {
-                    props.handleDeleteOption(props.option)
-                }}
-            >remove
-            </button>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        {props.option}
+        <button
+            onClick={() => {
+                props.handleDeleteOption(props.option)
+            }}
+        >remove
+        </button>
+    </div>
+);
 
 export default Option;
